07a77d89b3a052af1cba3f50ceda9d1c3a457cd1
zino uche <emazino@yahoo.com>
2023-10-05 13:43:57 -0300

    mod

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "aws" {}
 
-#resource "aws_vpc" "test" {
-# cidr = "10.0.0.0/16"
-#}
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Actions-test"
+  }
+}
